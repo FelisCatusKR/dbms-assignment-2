d0fe4a40ce359ec27dc570ea97ee2b7f6d2c3166
Hansaem Woo <wannaheal.kr@gmail.com>
2019-11-24 03:28:59 +0900

    Enhance frontend

--- src/static/index.js
+++ src/static/index.js
@@ -2,36 +2,33 @@ var curPage = 0;
 
 $("#search-button").click(function(event) {
   event.preventDefault();
-  var words = $("#search-input").val();
-  var url = "/api/users/";
+  var keyword = $("#search-input").val();
+  var url = "/api/users/" + "?name=" + keyword;
   curPage = 0;
-  url += "?name=" + words;
-  if (words != "") {
+  if (keyword != "") {
     $.ajax(url)
       .done(function(result) {
         if (result.number > 0) {
-          var thead = `<tr>
-                        <th scope="col" class="sr-only">UID</th>
-                        <th scope="col">#</th>
-                        <th scope="col">이름</th>
-                        <th scope="col">전화번호</th>
-                        <th scope="col"><span class="sr-only">수정/삭제</span></th>
-                      </tr>`;
-          $("#table-head").html(thead);
-
+          $("#table-head").prop("hidden", false);
           var str = "";
           for (var i = 0; i < result.user_list.length; i++) {
-            str += '<tr id="user-' + (i + 1) + '">';
+            str += '<tr class="tr-user" id="user-' + (i + 1) + '">';
             str +=
-              '<td class="user-id sr-only">' +
+              '<td class="tr-user-uid" hidden>' +
               result.user_list[i]["id"] +
               "</td>";
-            str += '<th scope="row">' + (i + 1) + "</th>";
-            str += "<td>" + result.user_list[i]["name"] + "</td>";
-            str += "<td>" + result.user_list[i]["phone_number"] + "</td>";
+            str += '<th class="tr-user-index" scope="row">' + (i + 1) + "</th>";
+            str +=
+              "<td class='tr-user-name'>" +
+              result.user_list[i]["name"] +
+              "</td>";
+            str +=
+              "<td class='tr-user-tel'>" +
+              result.user_list[i]["phone_number"] +
+              "</td>";
             str += `<td><div class="btn-group" role="group" aria-label="Action buttons">
-                      <button type="button" class="btn btn-warning">수정</button>
-                      <button type="button" class="btn btn-danger">삭제</button>
+                      <button type="button" class="btn btn-warning btn-update">수정</button>
+                      <button type="button" class="btn btn-danger btn-delete">삭제</button>
                     </div></td>`;
             str += "</tr>";
           }
@@ -71,4 +68,162 @@ $("#search-button").click(function(event) {
     $("#results").html("");
     $("#page-navigator").html("");
   }
+});
+
+$("#nav-user-add").click(function(event) {
+  event.preventDefault();
+  $("#modal-user-create").modal("show");
+});
+
+$("#user-create-form").submit(function(event) {
+  event.preventDefault();
+
+  $("#user-create-button").prop("disabled", true);
+  $("#user-create-button").html(`
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <span class="sr-only">처리중</span>
+  `);
+
+  var response = $.ajax("/api/users/", {
+    method: "POST",
+    data: JSON.stringify({
+      name: $("#user-create-form-name").val(),
+      phone_number: $("#user-create-form-tel").val()
+    })
+  });
+
+  $("#user-create-button").prop("disabled", false);
+  $("#user-create-button").html("추가");
+
+  response.always(function(data) {
+    if (data.status === 422) {
+      console.log(data.responseJSON);
+    } else {
+      $("#user-create-form-name").val("");
+      $("#user-create-form-tel").val("");
+      $("#modal-user-create").modal("hide");
+      $("#alert-placeholder").html(`
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          연락처 등록에 성공했습니다.
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      `);
+    }
+  });
+});
+
+$(document).on("click", ".btn-update", function() {
+  $("#user-update-form-uid").val(
+    $(this)
+      .parent()
+      .parent()
+      .parent()
+      .children(".tr-user-uid")
+      .text()
+  );
+  $("#user-update-form-name").val(
+    $(this)
+      .parent()
+      .parent()
+      .parent()
+      .children(".tr-user-name")
+      .text()
+  );
+  $("#user-update-form-tel").val(
+    $(this)
+      .parent()
+      .parent()
+      .parent()
+      .children(".tr-user-tel")
+      .text()
+  );
+  $("#modal-user-update").modal("show");
+});
+
+$("#user-update-form").submit(function(event) {
+  event.preventDefault();
+
+  $("#user-update-button").prop("disabled", true);
+  $("#user-update-button").html(`
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <span class="sr-only">처리중</span>
+  `);
+
+  var url = "/api/users/" + $("#user-update-form-uid").val();
+  var response = $.ajax(url, {
+    method: "PUT",
+    data: JSON.stringify({
+      name: $("#user-update-form-name").val(),
+      phone_number: $("#user-update-form-tel").val()
+    })
+  });
+
+  $("#user-update-button").prop("disabled", false);
+  $("#user-update-button").html("수정");
+
+  response.always(function(data) {
+    if (data.status === 422 || data.status === 405) {
+      console.log(data.responseJSON);
+    } else {
+      $("#user-update-form-uid").val("");
+      $("#user-update-form-name").val("");
+      $("#user-update-form-tel").val("");
+      $("#modal-user-update").modal("hide");
+      $("#alert-placeholder").html(`
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          연락처 수정에 성공했습니다.
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      `);
+    }
+  });
+});
+
+$(document).on("click", ".btn-delete", function() {
+  $("#modal-user-delete-uid").html(
+    $(this)
+      .parent()
+      .parent()
+      .parent()
+      .children(".tr-user-uid")
+      .text()
+  );
+  $("#modal-user-delete").modal("show");
+});
+
+$("#user-delete-button").click(function() {
+  $("#user-delete-button").prop("disabled", true);
+  $("#user-delete-button").html(`
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <span class="sr-only">처리중</span>
+  `);
+
+  var url = "/api/users/" + $("#modal-user-delete-uid").text();
+  var response = $.ajax(url, {
+    method: "DELETE"
+  });
+
+  $("#user-delete-button").prop("disabled", false);
+  $("#user-delete-button").html("삭제");
+
+  response.always(function(data) {
+    if (data.status === 422 || data.status === 405) {
+      console.log(data.responseJSON);
+    } else {
+      $("#modal-user-delete-uid").val("");
+      $("#modal-user-delete").modal("hide");
+      $("#alert-placeholder").html(`
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          연락처 삭제에 성공했습니다.
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      `);
+    }
+  });
 });
