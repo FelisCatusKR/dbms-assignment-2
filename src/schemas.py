6904dc60ecf6a86f05edf92b4d94a41a0cc02f3b
Hansaem Woo <wannaheal.kr@gmail.com>
2019-11-24 03:27:59 +0900

    Change Schemes for regex

--- src/schemas.py
+++ src/schemas.py
@@ -1,11 +1,10 @@
 from typing import List
-
-from pydantic import BaseModel
+from pydantic import BaseModel, Schema
 
 
 class UserBase(BaseModel):
-    name: str
-    phone_number: str
+    name: str = Schema(None, min_length=1)
+    phone_number: str = Schema(None, regex="010\d\d\d\d\d\d\d\d")
 
 
 class User(UserBase):
